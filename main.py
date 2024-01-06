e731b377bec0bd5f5a72cb72506a75382d21f676
fionn <fionnferreira@gmail.com>
2024-01-06 19:16:54 +0100

    host update

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ app.title = 'SpectroVolt'
 callbacks.register_callbacks(app)
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(debug=True, host='127.0.0.1', port=8080)
