3bd2bb4fd1742de6e56ccdd490a444998f1f5345
fionn <fionnferreira@gmail.com>
2024-01-09 14:49:31 +0100

    lan address printout working.

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ app.title = 'AkkuSpin'
 
 callbacks.register_callbacks(app)
 
-# Get the machine's local IP address
 local_ip = socket.gethostbyname(socket.gethostname())
 
-print(f'Server starting, Local address: http://127.0.0.1:8080/')
+print(f'Local address: http://127.0.0.1:8080/')
 print(f'Network address (accessible within your LAN): http://{local_ip}:8080')
 
 if __name__ == '__main__':
