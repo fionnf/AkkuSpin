5c2e4ae58f4fe78b0d2247a97d49cc97c4b97da0
F.M. Eckardt Ferreira <f.m.eckardt.ferreira@student.rug.nl>
2024-03-29 10:56:06 +0100

    bug fixes

--- layout.py
+++ layout.py
@@ -43,10 +43,10 @@ def create_layout():
                 html.H2("Global Settings", style={'font-family': sans_serif_font, 'font-size': '18px', 'line-height': '1.5', 'padding': '10px', 'margin-top': '0'}),
                 html.Label("NMR Folder Path:", style={'font-family': sans_serif_font, 'font-size': '16px'}),
                 dcc.Input(id='nmr_folder_input', type='text', placeholder='Enter NMR Folder Path',
-                          value=r'Test/NMR Folder', style=input_style),
+                          value=r'N:\anorg\s3941868\FF060', style=input_style),
                 html.Label("Voltage Folder Path:", style={'font-family': sans_serif_font, 'font-size': '16px'}),
                 dcc.Input(id='voltage_folder_input', type='text', placeholder='Enter Voltage Folder Path',
-                          value=r'Test/Cyclerfolder', style=input_style),
+                          value=r'X:\My Documents\Data\BioLogic\FF060', style=input_style),
                 html.Label("NMR Format:", style={'font-family': sans_serif_font, 'font-size': '16px'}),
                 dcc.RadioItems(
                     id='nmr_format_selector',
